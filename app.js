4ffa4c5003e4ceea85f4eb31b824b68441d7fde4
Lisa Anne Burr <burr_lisa@bah.com>
2017-04-06 16:18:07 -0400

    added textarea, cleaning code

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const vowels = /[aeiou]/i //using regex to define vowel set that ignores case
+//using regex to define vowel set that ignores case
+const vowels = /[aeiou]/i
 
-var str = "How are you doing today children?"
+//using regex to define 2-letter blends set that ignores case
+const blends2 = /(bl|br|ch|cl|cr|dr|fl|fr|gl|gr|pl|pr|sc|sh|sk|sl|sm|sn|sp|st|sw|th|tr|tw|wh|wr)/i
 
-//var result = str.match(vowels)
-//console.log(result)
+//using regex to define 3-letter blends set that ignores case
+const blends3 = /(sch|scr|shr|sph|spl)/i
 
-var wrd = str.split(/\s*\b\s*/)
-//wrd is the variable assigned the array of the words from the string
-console.log(wrd)
-
-
+//using regex to define 3 letter blends set that ignores case
+const punctuation = /[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g
 
 
+let plainWords = "Welcome to the wonderful world of Pig Latin."
+let wordArray = plainWords.split(/\s*\b\s*/)
 
 
 angular
@@ -19,8 +20,55 @@ angular
   .controller("piglatinsCtrl", [ piglatinController ] )
 
 function piglatinController () {
-  this.plainWords = str
-  this.wrd = str.split(/\s*\b\s*/)
-    console.log(wrd)
 
+  for (i = 0; i < wordArray.length; i++) {
+    var lettersArray = wordArray[i].split("")
+    var ltrs3 = (lettersArray[0] + lettersArray[1] + lettersArray[2])
+    var ltrs2 = (lettersArray[0] + lettersArray[1])
+
+    if ( ltrs3.match(blends3) ) {
+    ( lettersArray.push(ltrs3 + "ay") && lettersArray.splice(0, 3) )
+//      console.log(lettersArray)
+      var newWord = lettersArray.join("")
+      wordArray.splice(i,1,newWord)
+//                console.log(newWord)
+    } else {
+
+      if (ltrs2.match(blends2) ) {
+        ( lettersArray.push(ltrs2 + "ay") && lettersArray.splice(0,2) )
+//          console.log(lettersArray)
+          var newWord = lettersArray.join("")
+          wordArray.splice(i,1,newWord)
+//                console.log(newWord)
+      } else {
+
+        if ( lettersArray[0].match(vowels) ) {
+          ( lettersArray.push("ay") )
+//            console.log(lettersArray)
+            var newWord = lettersArray.join("")
+            wordArray.splice(i,1,newWord)
+//                console.log(newWord)
+        } else {
+
+          if ( lettersArray[0].match(punctuation) ) {
+            ( lettersArray )
+//              console.log(lettersArray)
+          } else {
+
+            ( lettersArray.push(lettersArray[0] + "ay") && lettersArray.splice(0, 1) )
+//              console.log(lettersArray)
+              var newWord = lettersArray.join("")
+              wordArray.splice(i,1,newWord)
+//                console.log(newWord)
+          }
+        }
+      }
+    }
+  }
+
+//    console.log(wordArray)
+
+  var piglatWords = wordArray.join(" ")
+    console.log(piglatWords)
+  this.piglatWords = piglatWords
 }
